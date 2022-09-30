f711df9b3adff0a1b805358fc55e4a860cd40589
rufus hamby <rufus.hamby@gmail.com>
2022-09-30 12:45:42 -0500

    syntax

---
+++ syntax/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace syntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 34;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than nine";
+            Console.WriteLine(response);
+
+        }
+    }
+}
+
